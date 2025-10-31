e5bd9665d45f38fbf708934ce60a727c7c3973b6
ntorres2013 <ntorres2013@sdsu.edu>
2025-10-30 20:28:46 -0700

    Update heap.h

--- heap.h
+++ heap.h
@@ -14,8 +14,28 @@ struct MinHeap {
 
     MinHeap() { size = 0; }
 
-    void push(int idx, int weightArr[]) {
+    void push(int idx, int weightArr[])
+    {
         // TODO: insert index at end of heap, restore order using upheap()
+//        Case 1: Heap is full
+        if(size >= 64)
+        {
+            cout << "Heap is full" << endl;
+            return;
+        }
+
+//        Case 2: Heap is empty, index getting pushed becomes the root
+        if(size == 0)
+        {
+            data[0] = idx;
+            size++;
+            return;
+        }
+
+//        Case 3: Heap is not empty, push at the end and upheap
+        data[size] = idx;
+        upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
